19982c37a609ec9b1532a0fbce4b3f2774a324ae
JeeshaanBakshi <86705915+JeeshaanBakshi@users.noreply.github.com>
2025-10-24 11:44:18 +0530

    Add files via upload

---
+++ RADAR.ino
@@ -0,0 +1,56 @@
+#include <Servo.h>
+
+const int trigPin = 10;
+const int echoPin = 11;
+long duration;
+int distance;
+Servo myServo;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  myServo.attach(12);
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  for (int i = 15; i <= 165; i++) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+
+  for (int i = 165; i > 15; i--) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+}
+
+int calculateDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  if (distance > 40 || distance == 90) {
+    digitalWrite(13, 1);
+    digitalWrite(8, 0);
+  } else {
+    digitalWrite(13, 0);
+    digitalWrite(8, 1);
+  }
+  return distance;
+}
